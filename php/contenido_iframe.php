7ce1714bf2a246c2e50dbbf25713fff4ed4289c0
Harold Espinoza <haroldesptru@gmail.com>
2020-06-12 21:35:04 -0600

    Corrección dentro del script 'contenido_iframe.php', ahora el script realiza la redirección al index dentro dentro de la instancia del proyecto en lugar de la raíz del host

--- php/contenido_iframe.php
+++ php/contenido_iframe.php
@@ -1,9 +1,12 @@
 <?php  
 
 $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-$serverhost = $_SERVER['HTTP_HOST'];
+$serverhost = $protocol.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$current = "php/".basename($_SERVER["SCRIPT_FILENAME"]);
 
-$path = $protocol.$serverhost.'/index.html';
+$fullpath = substr($serverhost, 0, -(strlen($current)));
+
+$path = $fullpath.'index.html';
 
 header('Location: '.$path);
 die();
